f9d7a5c0ffd8cee5c5d4b8e65acf51fc24eb05a8
jaelson_jacinto <jaelsonjacinto@outlook.com>
2021-11-25 10:41:50 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gerenciador-de-senhas',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
